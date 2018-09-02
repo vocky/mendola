456e67c0e50dc59c9983f57b832df7236121bf60
laye <182002323@qq.com>
2018-09-02 11:11:58 +0800

    add setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = 'laye'
+__version__ = "0.0.1"
+__copyright__ = ""
+__licence__ = "The MIT License"
+
+from setuptools import setup, find_packages
+
+setup(
+      name='mendola', 
+      version='0.0.1',
+      description='some useful python algorithms.',
+      author='Laye', 
+      author_email='wuch84@126.com',
+      url='https://github.com/vocky',
+      include_package_data=True,
+      zip_safe=False,
+      packages=find_packages()
+     )
